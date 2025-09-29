143642c744143628da57dfd4b77392b786dce218
Demenech <joaommdemenech@gmail.com>
2025-09-29 16:22:27 -0300

    feat(omd): rename OMD_TOKEN to DMS_TOKEN

--- lib/omd.ts
+++ lib/omd.ts
@@ -1,7 +1,7 @@
 export function omdFetch({ endpoint }: { endpoint: string }) {
   return fetch(`${process.env.NEXT_PUBLIC_DMS}/api/v1/${endpoint}`, {
     headers: {
-      authorization: `Bearer ${process.env.OMD_TOKEN}`,
+      authorization: `Bearer ${process.env.DMS_TOKEN}`,
     },
   });
 }
